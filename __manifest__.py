70f96cb544ff373aa77f09195ef09cdb9e77525f
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-19 12:28:33 +0100

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+  "name": "SID Stock Legacy",
+  "version": "15.0.1.0.0",
+  "depends": ["sid_stock_base", "sid_product_legacy"],
+  "data": [],
+  "installable": True,
+  "application": False
+}
